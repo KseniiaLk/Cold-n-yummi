879e589bef68be66110d73fd5fff5d2b39648a62
KseniiaLk <89646252+KseniiaLk@users.noreply.github.com>
2022-04-05 09:00:15 +0200

    tryed to fix database

--- models/index.js
+++ models/index.js
@@ -1,12 +1,40 @@
-const Sequelize = require('sequelize')
-const setupUser = require('./User')
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage:':memory:'
+const Data = require('../database');
+const User = require("./user");
+const Flavor = require("./flavor");
 
+Flavor.hasMany(User)
+User.belongsTo(Flavor)
+
+async function seed(){
+    await Flavor,bulkCreate([
+        {flavorName:"blueberry"},
+        {flavorName:"strawberry"},
+        {flavorName:"chocolate"},
+        {flavorName:"almond"},
+        {flavorName:"vanilla"},
+        {flavorName:"mango"},
+        {flavorName:"pineapple"},
+        {flavorName:"banana"},
+        {flavorName:"caramel"},
+        {flavorName:"mint"},
+        {flavorName:"raspberry"},
+        {flavorName:"cherry"},
+    ])
+}
+
+async function start(){
+    await setup()
+    await seed()
+}
+
+start()
+    
+ module.exports = {User,Flavor} 
+
+/*  sequelize.sync().then( info =>{
+    console.log(info);
 })
-
- const User =setupUser(sequelize)
-
- module.exports = {User,Choice}
+.catch((err)=>{
+console.log(err);
+}); */
